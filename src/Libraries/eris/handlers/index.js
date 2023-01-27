fdc45d7c050f42f26ad21be5f627a24e947cae8f
Kyle <37189894+eaobao@users.noreply.github.com>
2023-01-27 14:38:46 -0500

    Update index.js

--- src/Libraries/eris/handlers/index.js
+++ src/Libraries/eris/handlers/index.js
@@ -40,6 +40,9 @@ export { default as shardPreReady } from './ShardPreReadyHandler';
 export { default as shardReady } from './ShardReadyHandler';
 export { default as shardResume } from './ShardResumeHandler';
 
+export { default as threadCreate } from './ThreadCreateHandler;
+export { default as threadDelete } from './ThreadDeleteHandler;
+
 export { default as typingStart } from './TypingStartHandler';
 
 export { default as unavailableGuildCreate } from './UnavailableGuildCreateHandler';
